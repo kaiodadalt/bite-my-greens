279d36b924a0de87d2c4ced59dd602e75bb095d4
kaioebanx <kaiodadalt@gmail.com>
2025-01-12 15:39:57 -0300

    Adjusts on ChallengeGroupEntity

--- app/Domain/ChallengeGroup/Entities/ChallengeGroupEntity.php
+++ app/Domain/ChallengeGroup/Entities/ChallengeGroupEntity.php
@@ -8,26 +8,20 @@ use DateTime;
 class ChallengeGroupEntity implements Entity
 {
     public function __construct(
-        private ?int     $id = null,
-        private ?string  $name = null,
-        private ?DateTime  $end_date = null,
-        private ?int     $created_by = null,
-        private ?string  $created_at = null,
-        private ?string  $updated_at = null,
+        private readonly int    $id,
+        private string          $name,
+        private DateTime        $end_date,
+        private readonly int    $created_by,
+        private readonly string $created_at,
+        private readonly string $updated_at,
     ) {}
 
-    public function getId(): ?int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -38,7 +32,7 @@ class ChallengeGroupEntity implements Entity
         return $this;
     }
 
-    public function getEndDate(): ?DateTime
+    public function getEndDate(): DateTime
     {
         return $this->end_date;
     }
@@ -54,36 +48,18 @@ class ChallengeGroupEntity implements Entity
         return $this->created_by;
     }
 
-    public function setOwnerId(int $created_by): self
-    {
-        $this->created_by = $created_by;
-        return $this;
-    }
-
     public function hasOwner(int $user_id): bool
     {
         return $this->created_by === $user_id;
     }
 
-    public function getCreatedAt(): ?string
+    public function getCreatedAt(): string
     {
         return $this->created_at;
     }
 
-    public function setCreatedAt(string $created_at): self
-    {
-        $this->created_at = $created_at;
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?string
+    public function getUpdatedAt(): string
     {
         return $this->updated_at;
-    }
-
-    public function setUpdatedAt(string $updated_at): self
-    {
-        $this->updated_at = $updated_at;
-        return $this;
     }
 }
